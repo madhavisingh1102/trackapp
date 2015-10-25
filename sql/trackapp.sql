55f5aa39244de8ee249f8d3f0d89f1167d4ed239
madhavisingh1102 <madhavisingh1102@gmail.com>
2015-10-25 21:40:21 +0530

    updating schema file with working queries

--- sql/trackapp.sql
+++ sql/trackapp.sql
@@ -1,1 +1,26 @@
-create table userinfo;
+create table user_info (user_id int not null auto_increment,
+						name varchar(20) not null,
+						salary float,
+						DOB date,
+						primary key (user_id))
+						);
+create table user_track_data (id int not null auto_increment,
+							  user_id int not null,
+							  amount float,
+							  category int,
+							  sub_category int,
+							  description varchar(40),
+							  );
+create table sub_category (id int not null auto_increment,
+					       sub_cat_description varchar(40),
+					       );
+create table category (id int not null auto_increment,
+					   cat_description varchar(40)
+				       );
+create table user_saving_data (id int not null auto_increment,
+							   user_id int,
+							   saving_type varchar(20),
+							   amount float,
+							   description varchar(40),
+							   maturity_date date,
+							   )
