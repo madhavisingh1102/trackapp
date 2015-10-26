ba6333990a1508688381eaf171301fa53385e898
Madhavi <Madhavi>
2015-10-26 22:43:05 +0530

    adding servlet class for uer login

--- java/com/madhavi/trackapp/servlet/LoginServlet.java
+++ java/com/madhavi/trackapp/servlet/LoginServlet.java
@@ -1,5 +1,13 @@
 package com.madhavi.trackapp.servlet;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class LoginServlet extends HttpServlet
+{
 
-public class LoginServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+	{
+	}
 
 }
+
