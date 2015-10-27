fd6a7e71ca7405aaad2ab3d350524f6a8bdb8a5a
madhavisingh1102 <madhavisingh1102@gmail.com>
2015-10-27 12:57:47 +0530

    Create RegisterUser.java

---
+++ java/com/madhavi/trackapp/servlet/RegisterUser.java
@@ -0,0 +1,14 @@
+package com.madhavi.trackapp.servlet;
+import java.io.*;
+import java.servlet.*;
+import java.servlet.http;
+public class RegisterUser extends HttpServlet
+{
+public void doGet(HttpServletRequest req, HttpServletResponse res)
+  {
+    Once user click on register tab, then return form.
+    get all the user details from form.
+    insert all user data to database user table.
+    After inserting data in DB return User welcome page.
+  }
+}
