137cc3c88b12e6ee039bf73b5623c859500b142f
madhavisingh1102 <madhavisingh1102@gmail.com>
2015-10-27 11:09:47 +0530

    algo for user login

--- java/com/madhavi/trackapp/servlet/LoginServlet.java
+++ java/com/madhavi/trackapp/servlet/LoginServlet.java
@@ -7,6 +7,11 @@ public class LoginServlet extends HttpServlet
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 	{
+		//get user id and password.
+		//get user id and password from database.
+		// compare DB details with entered user details.
+		//if true then return home page else return error page.
+		
 	}
 
 }
