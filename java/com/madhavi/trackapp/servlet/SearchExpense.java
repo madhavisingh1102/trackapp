2a23bc9e3a5d90900a95180c74b375cd25deb6f7
madhavisingh1102 <madhavisingh1102@gmail.com>
2015-11-04 16:52:44 +0530

    Update SearchExpense.java

--- java/com/madhavi/trackapp/servlet/SearchExpense.java
+++ java/com/madhavi/trackapp/servlet/SearchExpense.java
@@ -5,7 +5,9 @@ import javax.servlet.http.*;
 public class SearchExpense extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 	{
-		
+		//After clicking the search tab. Return search page
+		// display search crieteria drop down. Display date,
+		//get parameter 
 	}
 
 }
