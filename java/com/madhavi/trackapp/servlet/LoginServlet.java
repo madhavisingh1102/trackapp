e039b60447e3c6f5a4dd3eb096f1b098a5d6d98f
madhavi <madhavi@madhavis-MacBook-Air.local>
2016-07-22 00:32:22 +0530

    java/com/madhavi/trackapp/servlet/LoginServlet.java

--- java/com/madhavi/trackapp/servlet/LoginServlet.java
+++ java/com/madhavi/trackapp/servlet/LoginServlet.java
@@ -2,18 +2,28 @@ package com.madhavi.trackapp.servlet;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 public class LoginServlet extends HttpServlet
 {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 	{
 		//get user id and password.
-		String User = request.getParameter ("username");
-		String pwd = request.getParameter ("password");
+		String User = req.getParameter ("username");
+		String pwd = req.getParameter ("password");
 		//get user id and password from database.
-		
-		// compare DB details with entered user details.
-		//if true then return home page else return error page.
+		if (Validate.CheckUser(User,pwd))
+		{
+			RequestDispatcher rd = req.getRequestDispatcher("Welcome");
+			rd.forward(req,res);
+		}
+		else
+		{
+			System.out.println("User and Password in incorrect");
+			RequestDispatcher rd = req.getRequestDispatcher("LoginFront.jsp");
+			rd.include(req,res);
+
+		}
 		
 	}
 
