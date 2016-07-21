4166611914b8281dc0f4d6c857724740f18c7ffa
madhavisingh1102 <madhavisingh1102@gmail.com>
2016-07-21 18:06:16 +0530

    Update LoginServlet.java

--- java/com/madhavi/trackapp/servlet/LoginServlet.java
+++ java/com/madhavi/trackapp/servlet/LoginServlet.java
@@ -5,10 +5,13 @@ import javax.servlet.http.*;
 public class LoginServlet extends HttpServlet
 {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 	{
 		//get user id and password.
+		String User = request.getParameter ("username");
+		String pwd = request.getParameter ("password");
 		//get user id and password from database.
+		
 		// compare DB details with entered user details.
 		//if true then return home page else return error page.
 		
