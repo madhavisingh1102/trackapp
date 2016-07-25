f40f87bfa13e9c92a9312cb516e80b39fc9e35c3
madhavisingh1102 <madhavisingh1102@gmail.com>
2016-07-25 17:11:50 +0530

    Update Validate.java

--- java/com/madhavi/trackapp/Validate.java
+++ java/com/madhavi/trackapp/Validate.java
@@ -1,36 +1,38 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Validate {
 
 public static void main(String args[])
 {
-  try 
-  {
-         
-        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-        stmt = conn.createStatement();
-        String db_username = null;
-        String db_password = null;
-            query = "SELECT username, password FROM login;";
-            stmt.executeQuery(query);
-            ResultSet rs = stmt.getResultSet();
-
-            while(rs.next()){
-                db_username = rs.getString("username");
-                db_password = rs.getString("password");
-                //check null's also 
-                if(dbUsername.equals(username) && dbPassword.equals(password)){
-                   System.out.println("sucess");
-
-                }
-
-            }
-        }  catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
+   try
+   {
+   String host = "address";
+   String dbusername = "Madhavi";
+   String dbpassword = "mad#123";
+   
+   Connection con = DriverManger.getConnection(host,dbusername,dbpassword);
+   public boolean checkUser(String user, String pwd)
+   {
+   Statement stmt = con.createStatement();
+   Result rs = stmt.executeQuesry("Select user_name and password from user_validation");
+   if (user.equals(user_name))
+   {
+     if (pwd.equals(password))
+    return true; 
+     
+   }
+   
+   }
+   catch (SQLException err)
+   {
+     System.out.println(err.getMessage())
+   }
+   }
+   finally()
+   {
+     out.close();
+   }
+}
+}
