2be0daa633ff7d5374ed2a8ca9a5955aea786fcd
madhavisingh1102 <madhavisingh1102@gmail.com>
2016-09-12 11:23:09 +0530

    Create user.java

---
+++ java/com/madhavi/trackapp/model/user.java
@@ -0,0 +1,23 @@
+public class user 
+{
+  String name;
+  String email;
+  
+  public String getName ()
+  {
+    return name;
+  }
+  public String getEmail()
+  {
+    return email;
+  }
+  
+  public void setName (String name)
+  {
+    this.name = name;
+  }
+  public void setEmail (String email)
+  {
+    this.email = email;
+  }
+}
