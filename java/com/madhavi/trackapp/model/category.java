f7c3da48aa1f3ee1b63c1cecf74bd09533f20b4f
madhavisingh1102 <madhavisingh1102@gmail.com>
2016-09-12 12:19:41 +0530

    Create category.java

---
+++ java/com/madhavi/trackapp/model/category.java
@@ -0,0 +1,25 @@
+public category 
+{
+  String catname;
+  String description;
+  
+  public String getCatname ()
+  {
+    return catname;
+  }
+  public String getDescription ()
+  {
+    return description;
+  }
+  
+  public void setCatname (String catname)
+  {
+    this.catname = catname; 
+  }
+  
+  public void setDescription (String description)
+  {
+    this.description = description;
+  }
+}
+   
