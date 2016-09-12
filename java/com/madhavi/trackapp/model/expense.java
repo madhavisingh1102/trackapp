8c05f69161bb906fa2a0d82f925b3d6fd7517290
madhavisingh1102 <madhavisingh1102@gmail.com>
2016-09-12 12:36:05 +0530

    Create expense.java

---
+++ java/com/madhavi/trackapp/model/expense.java
@@ -0,0 +1,16 @@
+public class expense
+{
+  float amount;
+  // how to enter the date. User should get the calender while entering the date.??
+  public float getAmount ()
+  {
+    return (amount);
+  }
+  public void setAmount (float amount)
+  {
+    this.amount = amount;
+  }
+  
+}
+
+  
